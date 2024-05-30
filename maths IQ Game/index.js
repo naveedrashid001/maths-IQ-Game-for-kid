1f204f807615a67692d2cc57e0db49eac85492d0
Naveed Rashid <149875690+naveedrashid001@users.noreply.github.com>
2024-05-30 15:14:54 -0700

    Add files via upload

---
+++ maths IQ Game/index.js
@@ -0,0 +1,9 @@
+// const AddplayerName = document.getElementById("AddplayerName")
+// const EnterName = window.prompt("Enter player Name !!");
+// if (EnterName) {
+//     AddplayerName.innerHTML += `${EnterName}`;
+// }
+// else {
+//     AddplayerName.innerHTML += "Player1";
+// }
+
