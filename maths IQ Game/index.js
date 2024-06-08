16438374b31a26857f9a14bf9ac90e01230d04f5
Naveed Rashid <149875690+naveedrashid001@users.noreply.github.com>
2024-06-08 04:32:35 -0700

    now user can enter player name

--- maths IQ Game/index.js
+++ maths IQ Game/index.js
@@ -1,9 +1,10 @@
-const AddplayerName = document.getElementById("AddplayerName")
-const EnterName = window.prompt("Enter player Name !!");
-if (EnterName) {
-    AddplayerName.innerHTML += `${EnterName}`;
-}
-else {
-    AddplayerName.innerHTML += "Player1";
-}
+const letsPlayButton = document.getElementById("letsPlayButton");
 
+letsPlayButton.addEventListener("click", function(event) {
+    const enterName = window.prompt("Enter player Name !!");
+    if (enterName) {
+        localStorage.setItem("playerName", enterName);
+    } else {
+        localStorage.setItem("playerName", "Player1");
+    }
+});
